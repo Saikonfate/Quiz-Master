709d6a38d96e5d9bde61eec2d2aa164f23d71c9f
Saikon <wesley_furtadopessoa@outlook.com>
2025-11-26 22:01:28 -0300

    Enhance UI styling for login and application components

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,16 +7,16 @@
 
         <!-- Email Address -->
         <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
+            <x-input-label for="email" :value="__('Email')" class="text-brand-light" />
+            <x-text-input id="email" class="block mt-1 w-full bg-brand-dark border-gray-600 text-brand-light focus:border-brand-yellow focus:ring-brand-yellow" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
         <!-- Password -->
         <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+            <x-input-label for="password" :value="__('Password')" class="text-brand-light" />
 
-            <x-text-input id="password" class="block mt-1 w-full"
+            <x-text-input id="password" class="block mt-1 w-full bg-brand-dark border-gray-600 text-brand-light focus:border-brand-yellow focus:ring-brand-yellow"
                             type="password"
                             name="password"
                             required autocomplete="current-password" />
@@ -27,19 +27,19 @@
         <!-- Remember Me -->
         <div class="block mt-4">
             <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
-                <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+                <input id="remember_me" type="checkbox" class="rounded border-gray-600 bg-brand-dark text-brand-yellow shadow-sm focus:ring-brand-yellow" name="remember">
+                <span class="ms-2 text-sm text-gray-400">{{ __('Remember me') }}</span>
             </label>
         </div>
 
         <div class="flex items-center justify-end mt-4">
             @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
+                <a class="underline text-sm text-gray-400 hover:text-brand-yellow rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-brand-yellow" href="{{ route('password.request') }}">
                     {{ __('Forgot your password?') }}
                 </a>
             @endif
 
-            <x-primary-button class="ms-3">
+            <x-primary-button class="ms-3 bg-brand-yellow text-brand-dark hover:bg-yellow-400 focus:bg-yellow-400 active:bg-yellow-500">
                 {{ __('Log in') }}
             </x-primary-button>
         </div>
